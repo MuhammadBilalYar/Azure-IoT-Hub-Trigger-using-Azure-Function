8059b99fa2228aae56a8f8020df8b5f65481f59d
Muhammad Bilal Yar <MuhammadBilalYar@users.noreply.github.com>
2021-04-22 18:57:29 +0500

    Fix Iot Hub issues

--- Test/index.ts
+++ Test/index.ts
@@ -1,11 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import AzureServiceBusClient from "../Clients/AzureServiceBusClient";
-import { ServiceBusMessage } from "@azure/service-bus";
 import AzureStorageClient from "../Clients/AzureStorageClient";
 import Processor from "../Clients/Processor";
 import Config from "../Config";
-import { config } from "node:process";
-const AWS = require('aws-sdk');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
