64c82c72ee63aec18cd121429ca10c9e31c0f2d2
Muhammad Bilal Yar <MuhammadBilalYar@users.noreply.github.com>
2021-04-22 18:26:52 +0500

    Tested changes

--- Test/index.ts
+++ Test/index.ts
@@ -4,25 +4,22 @@ import { ServiceBusMessage } from "@azure/service-bus";
 import AzureStorageClient from "../Clients/AzureStorageClient";
 import Processor from "../Clients/Processor";
 import Config from "../Config";
+import { config } from "node:process";
+const AWS = require('aws-sdk');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const payload = req.body;
-    // const messages: ServiceBusMessage[] = [
-    //     { body: responseMessage }
-    // ];
-    // const client = new AzureServiceBusClient();
-    // await client.Send(messages);
 
-    // const client = new AzureStorageClient();
-    // const CERT = await client.GetBlob();
-    const x = new Buffer(Config.EncryptedCipherKey, 'base64')
-    // const payload = new Buffer(data, 'base64').toString('ascii');
-    // await Processor.Start(payload, { cipherKey: Config.EncryptedCipherKey, cert: CERT })
+    const payload = req.body;
+
+    const client = new AzureStorageClient();
+    const CERT = await client.GetBlob();
+
+    await Processor.Start(payload, { cipherKey: Config.EncryptedCipherKey, cert: CERT })
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: x
+        body: payload
     };
 
 };
